4348c349d408c1254f6a9bd39efc1f9e52bde0b9
Eric Long <i@hack3r.moe>
2023-07-25 02:38:19 +0800

    Initial commit

---
+++ debian/scripts/rebuild-native-node-extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_electron_ver=$1
+_arch=$2
+
+for _module in @vscode/spdlog @parcel/watcher keytar native-is-elevated native-keymap native-watchdog node-pty windows-foreground-love; do
+  pushd node_modules/$_module
+  node-gyp rebuild --target=$_electron_ver --arch=$_arch --dist-url=https://electronjs.org/headers --debug
+  mkdir build/Release
+  cp build/Debug/*.node build/Release
+  rm -rf build/Debug
+  popd
+done
